3fc5b6aa85aa11da9a907d2bdc6422fbf0f0cd2d
tony <tony@tonydeMacBook-Pro-2.local>
2024-05-26 07:36:15 +0800

    完成了tabbar 功能的开发

--- App.vue
+++ App.vue
@@ -17,7 +17,7 @@
 	/*每个页面公共css */
 	@import '@/uni_modules/uni-scss/index.scss';
 	/* #ifndef APP-NVUE */
-	@import '@/static/customicons.css';
+	
 	// 设置整个项目的背景色
 	page {
 		background-color: #f5f5f5;
@@ -25,7 +25,7 @@
 
 	/* #endif */
 	.example-info {
-		font-size: 14px;
+		font-size: 1px;
 		color: #333;
 		padding: 10px;
 	}
