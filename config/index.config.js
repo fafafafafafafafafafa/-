de98b20fea7c3d4e3b3f6496c1fb52417a102ed4
tony <tony@tonydeMacBook-Pro-2.local>
2024-05-29 22:17:00 +0800

    完成了首页功能的开发

---
+++ config/index.config.js
@@ -0,0 +1,15 @@
+{
+  "message": [
+    {
+      "image_src": "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/20%25-discount-poster-flyer-design-template-597b64096751cdda415a9b4cfc05ab39_screen.jpg?ts=1658740914",
+      "open_type": "navigate",
+      "goods_id": 129,
+      "navigator_url": "/pages/goods_detail/main?goods_id=129"
+    }
+    
+  ],
+  "meta": {
+    "msg": "获取成功",
+    "status": 200
+  }
+}
--- main.js
+++ main.js
@@ -1,7 +1,36 @@
-
-// #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+ 
+// 导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+
+// 请求的根路径
+$http.baseUrl = "https://api-hmugo-web.itheima.net"
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  // do somethimg...
+  uni.showLoading({
+	  title: 'loading...'
+  })
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+//封装弹框的方法
+uni.$showMsg = function(title = "数据请求失败！", duration = 1500){
+	uni.showToast({
+		title,
+		duration,
+		icon: "none"
+	})
+}
 
 Vue.config.productionTip = false
 
@@ -11,15 +40,5 @@ const app = new Vue({
     ...App
 })
 app.$mount()
-// #endif
 
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-import App from './App.vue'
-export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
-}
-// #endif
+
